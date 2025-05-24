3403aabd9c32630124baa58223c7f35b46f593c9
OldLeatherneck <4bigjx@gmail.com>
2025-05-24 07:55:25 -0400

    Updated xunzip to the latest version.

--- UIXLiteFTP/xunzip.h
+++ UIXLiteFTP/xunzip.h
@@ -1,5 +1,4 @@
-// Borrowed with permission from Crunchbite to unzip UIX-Lite resources and installation packages. :D
 
 // xUnzip is not yet ready for public release so you only get this header and a compiled static library for now. Sorry :(
 
-bool xExtractZip(const char *pszSource, const char *pszDestinationFolder, const bool bUseFolderNames, const bool bOverwrite);
+bool xExtractZip(const char * pszSource, const char * pszDestinationFolder, const bool bUseFolderNames, const bool bOverwrite, const bool bDeleteZip = false);
